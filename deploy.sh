dd00f3b7c28c6ecd6298285a54930ff8d79c4563
S4RMI618 <huessabe@gmail.com>
2023-08-06 11:01:55 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# git push -f git@github.com:S4RMI618/S4RMI618.github.io.git main:gh-pages
+git push -f git@github.com:S4RMI618/S4RMI618.github.io.git main:gh-pages
 
 cd -
